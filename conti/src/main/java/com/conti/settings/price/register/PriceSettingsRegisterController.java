fc6f14a073e3e87111ac7ae25192b1f0433840e7
Monu.C <monu.c@pointdot.in>
2017-08-01 12:13:37 +0530

    register pricesetting conflict

--- conti/src/main/java/com/conti/settings/price/register/PriceSettingsRegisterController.java
+++ conti/src/main/java/com/conti/settings/price/register/PriceSettingsRegisterController.java
@@ -33,7 +33,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.conti.config.SessionListener;
-import com.conti.master.location.Location;
 import com.conti.others.ConstantValues;
 import com.conti.others.Loggerconf;
 import com.conti.others.UserInformation;
@@ -42,6 +41,8 @@ import com.conti.settings.company.Company;
 import com.conti.settings.company.CompanySettingDAO;
 import com.conti.settings.price.PriceSetting;
 import com.conti.settings.price.PriceSettingDao;
+import com.conti.settings.price.PriceSettingDetail;
+import com.conti.settings.price.PriceSettingDetailDao;
 
 /**
  * @Project_Name conti
@@ -66,7 +67,8 @@ public class PriceSettingsRegisterController {
 	@Autowired
 	private CompanySettingDAO companySettingDAO;
 		
-	
+	@Autowired
+	private PriceSettingDetailDao psdDAo;
 	@Autowired
 	@Qualifier("sessionRegistry")
 	private SessionRegistry sessionRegistry;
@@ -103,7 +105,7 @@ public class PriceSettingsRegisterController {
 			to_limit = 10;
 		} else {
 			from_limit = (page * 10) + 1;
-			to_limit =  (page + 10 ) * 10;
+			to_limit =  (page + 1 ) * 10;
 		}	
 		List<PriceSetting> priceSettinglist=psDao.getPriceSettingWithLimit(from_limit, to_limit, order); 
 		return new ResponseEntity<List<PriceSetting>>(priceSettinglist,HttpStatus.OK);
@@ -237,5 +239,36 @@ public class PriceSettingsRegisterController {
 
 	}
 
-
+	
+	//--------------------------------- Fetch price by from branch OR to branch and service and product writen by sankar
+	@RequestMapping(value = "fetch_priceforShipment", method = RequestMethod.POST)
+	public ResponseEntity<String> fetch_priceforShipment (@RequestBody String product_info, HttpServletRequest request) {
+		
+		JSONObject json = new JSONObject(product_info);
+		int from_branch = json.getInt("from_branch_id");
+		int to_branch = json.getInt("to_branch_id");
+		int product = json.getInt("product_id");
+		int service = json.getInt("service_id");
+		int max_weight = json.getInt("max_weight");
+		
+		PriceSetting priceSetting = psDao.fetchprice(from_branch, product, service);
+		
+		if(priceSetting != null) {
+			if(priceSetting.getDefault_price() != 0 ) {
+				return new ResponseEntity<String>(Float.toString(priceSetting.getDefault_price()) , HttpStatus.OK);
+			} else {
+				PriceSettingDetail priceSettingDetail = psdDAo.fetchprice(priceSetting.getPricesetting_id(), to_branch, max_weight);
+				if( priceSettingDetail != null ) {
+					return new ResponseEntity<String>(Float.toString(priceSettingDetail.getPs_price()) , HttpStatus.OK);
+				} else {
+					return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+				}
+			}
+		} else {
+			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+		}
+		
+	}
+	
+	
 }
