9701c7c5a233b41418dec6271b5d5ee70ce90294
Suresh <suresh.p@pointdot.in>
2017-08-10 17:23:18 +0530

    receipt print updated

--- conti/src/main/webapp/resources/custom/js/receipt/receipt_controller.js
+++ conti/src/main/webapp/resources/custom/js/receipt/receipt_controller.js
@@ -12,23 +12,26 @@ contiApp.controller('ReceiptController',['$scope','$http','$q','$timeout','Recei
 {
 	var self=this;
 	self.receipts=[];
-	self.selected_receipts=[];
+	self.selected_receipt=[];
 	self.Filterreceipts=[];
-	self.branches=[];
+	self.receipts=[];
 	self.heading="Manster"
 	self.message=null;
 	self.print=print;
+	self.receiptSelect=receiptSelect;
+	self.receiptSelectAll=receiptSelectAll;
+	self.shownoofRecord=shownoofRecord;
+	$scope.shownoofrec=10;
 	
 	self.receiptSelectAll=receiptSelectAll;
 	fetchAllReceipt();
-	//fetchAllBranches();
+	fetchAllBranches();
 		
 	function reset()
 	{
 		self.manifest={};
 		self.heading='Master';
 		fetchAllManifest();
-		
 	}	
 
 	//-----------------------------New and close function---------------------------	
@@ -58,19 +61,22 @@ contiApp.controller('ReceiptController',['$scope','$http','$q','$timeout','Recei
 	
 	//-------------------------- Fetch All Branch begin ---------------------//	
 	
-	function fetchAllBranches() {
+	function fetchAllBranches() 
+	{
 		console.log("get all branches")
 		BranchService.fetchAllBranches()
-			.then(
-					function (branches) {
-						self.branches = branches;
-//						pagination();
-						console.log("get all branches "+self.branches)
-					}, 
-					function (errResponse) {
-						console.log('Error while fetching branches');
-					}
-				);
+		.then(
+				function (branches) 
+				{
+					self.branches = branches;
+					pagination();
+					//console.log("get all branches "+self.branches)
+				}, 
+				function (errResponse) 
+				{
+					console.log('Error while fetching branches');
+				}
+			);
 	}
 	
 	//-------------------------- Fetch All Branch end ---------------------//
@@ -119,9 +125,181 @@ contiApp.controller('ReceiptController',['$scope','$http','$q','$timeout','Recei
 		var index=self.selected_receipt.indexOf(receipt);
 		if(receipt.select)
 		{
-			self.selected_receipt.push(re)
+			self.selected_receipt.push(receipt);
+		}
+		else
+		{
+			self.selected_receipt.splice(index,1);
 		}
 	}
+	//---------------------------------------------------------------------------------------------
+
+	//--------------------------------------Show number of records-------------------------------------
 	
+	function shownoofRecord()
+	{
+		$scope.pageSize=$scope.shownoofrec;
+		self.Filterreceipts=self.receipts.slice($scope.currentPage*$scope.pageSize);
+		if(self.Filterreceipts.length<=$scope.pageSize)
+		{
+			$scope.previousDisabled=true;
+			$scope.nextDisabled=true;
+		}
+		else
+		{
+			$scope.nextDisabled=false;
+		}
+	}
+	//------------------------------------------------------------------------------------------------
+	
+	//----------------------------------------Register Search-----------------------------------------
+	
+	function registerSearch(searchkey)
+	{
+		if(searchkey.length==0)
+		{
+			self.Filterreceipts=self.receipts;
+		}
+		else if(searchkey.length>3)
+		{
+			Branch
+		}
+	}
+	//----------------------------------------------------------------------------------------------------
+	
+	//-----------------------Record count begin-------------------------------------
+	
+	function findrecord_count()
+	{
+		BranchService.findrecord_count()
+		.then(
+				function(record_count)
+				{
+					console.log(record_count);
+					$scope.totalnof_record_count;
+				},
+				function(errResponse)
+				{
+					console.log('Error while fetching record count....');
+				}
+			);
+	}
+	//--------------------------------------------------------------------------------
+	
+	//------------------------Receipt Pagination--------------------------------------
+	
+	function pagination()
+	{
+		$scope.pageSize=$scope.shownoofrec;
+		$scope.currentPage=0;
+		$scope.totalPages=0;
+		self.Filterreceipts=self.receipts;
+		$scope.nextDisabled=false;
+		$scope.previouseDisabled=true;
+		if(self.Filterreceipts.length<=10)
+		{
+			$scope.nextDisabled=true;
+		}
+		if(self.Filterreceipts.length<100)
+		{
+			$scope.totalnof_records=self.Filterreceipts.length;
+		}
+		else
+		{
+			findrecord_count();
+		}
+	}
+	//---------------------------------------------------------------------------------
+	
+	//-----------------------Paginate function start----------------------------------
+	
+	$scope.paginate=function(nextPrevMultiplier)
+	{
+		$scope.selectallreceipts=false;
+		$scope.currentPage+=(nextPrevMultiplier*1);
+		self.Filterreceipts=self.receipts.slice($scope.currentPage*$scope.pageSize);
+		console.log(self.Filterreceipts.length);
+		if(self.Filterreceipts.length==0)
+		{
+			BranchService.pagination_byPage($scope.currentPage)
+			.then(
+					function(filterReceipt)
+					{
+						if(filterReceipt.length==0)
+						{
+							$scope.nextDisabled=true;
+						}
+						else if(filterReceipt.length<10)
+						{
+							self.Filterreceipts=filterReceipt;
+							$scope.nextDisabled=true;
+						}
+						else
+						{
+							self.Filterreceipts=filterReceipt;
+						}
+					},
+					function(errResponse)
+					{
+						console.log('Error while pagination...');
+					}
+				);
+		}
+		if(self.Filterreceipts.length<$scope.pageSize)
+		{
+			$scope.nextDisabled=true;
+		}
+		if($scope.currentPage==0)
+		{
+			$scope.previouseDisabled=true;
+		}
+		if(nextPrevMultipier==-1)
+		{
+			$scope.nextDisabled=false;
+		}
+		else
+		{
+			$scope.previouseDisabled=false;
+		}
+	}
+	//------------------------------------------------------------------------------------
+	
+	//-----------------------------First and Last Pagination------------------------------------
+	
+	$scope.firstlastPaginate=function(page)
+	{
+		$scope.selectallreceipts=false;
+		if(page==1)
+		{
+			$scope.currentPage=0;
+			$scope.previouseDisabled=true;
+			$scope.nextDisabled=false;
+			self.Filterreceipts=self.receipts.slice($scope.currentPage*$scope.pageSize);
+			fetchAllReceipt();
+		}
+		else
+		{
+			$scope.currentPage=((Math.cell(self.Filterreceipts.length/$scope.pageSize))-1);
+			$scope.previouseDisabled+false;
+			$scope.nextDisabled=true;
+			self.Filterreceipts=self.receipts.slice($scope.currentPage*$scope.pageSize)
+			console.log(self.Filterreceipts.length);
+			if(self.Filterreceipt.length==0)
+			{
+				BranchService.pagination_byPage(page)
+				.then(
+						function(filterReceipt)
+						{
+							self.Filterreceipts=filterReceipt;
+						},
+						function(errResponse)
+						{
+							console.log('Error while fetching Branch');
+						}
+					);
+			}
+		}
+	}
+	//-----------------------------------------------------------------------------------------------
 	}
 ]);
